1829432ecf424cb25772217184f9c1b71d7edd50
Michael <mmbmf.mm@gmail.com>
2020-01-01 20:23:34 +0000

    POST testing complete

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -24,6 +24,26 @@ articlesRouter
   .post(jsonParser, (req, res, next) => {
     const { title, content, style } = req.body;
     const newArticle = { title, content, style };
+
+    // if (!title) {
+    //   return res.status(400).json({
+    //     error: { message: `Missing 'title' in request body` }
+    //   });
+    // }
+
+    // if (!content) {
+    //   return res.status(400).json({
+    //     error: { message: `Missing 'content' in request body` }
+    //   });
+    // }
+
+    for (const [key, value] of Object.entries(newArticle)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        })
+      }
+    }
     ArticlesService.insertArticle(req.app.get("db"), newArticle)
       .then(article => {
         res
--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -70,7 +70,7 @@ describe("Articles Endpoints", function() {
     });
   });
 
-  describe(`POST /articles`, () => {
+  describe.only(`POST /articles`, () => {
     it(`creates an article, responding with 201 and the new article`, function() {
       this.retries(3);
       const newArticle = {
@@ -88,7 +88,7 @@ describe("Articles Endpoints", function() {
           expect(res.body.content).to.eql(newArticle.content);
           expect(res.body).to.have.property("id");
           expect(res.headers.location).to.eql(`/articles/${res.body.id}`);
-          const expected = new Date().toLocaleString('en', { timeZone: 'UTC' });
+          const expected = new Date().toLocaleString("en", { timeZone: "UTC" });
           const actual = new Date(res.body.date_published).toLocaleString();
           expect(actual).to.eql(expected);
         })
@@ -97,6 +97,63 @@ describe("Articles Endpoints", function() {
             .get(`/articles/${postRes.body.id}`)
             .expect(postRes.body)
         );
+    });
+
+    // it(`responds with 400 and an error message when the 'title' is missing`, () => {
+    //   return supertest(app)
+    //     .post("/articles")
+    //     .send({
+    //       style: "Listicle",
+    //       content: "Test new article content..."
+    //     })
+    //     .expect(400, {
+    //       error: { message: `Missing 'title' in request body` }
+    //     });
+    // });
+
+    // it(`responds with 400 and an error message when the 'content' is missing`, () => {
+    //   return supertest(app)
+    //     .post("/articles")
+    //     .send({
+    //       title: "Test new article",
+    //       style: "Listicle"
+    //     })
+    //     .expect(400, {
+    //       error: { message: `Missing 'content' in request body` }
+    //     });
+    // });
+
+    // it(`responds with 400 and an error message when the 'style' is missing`, () => {
+    //   return supertest(app)
+    //     .post("/articles")
+    //     .send({
+    //       title: "Test new article",
+    //       content: "Test new article content..."
+    //     })
+    //     .expect(400, {
+    //       error: { message: `Missing 'style' in request body` }
+    //     });
+    // });
+
+    const requiredFields = ["title", "style", "content"];
+
+    requiredFields.forEach(field => {
+      const newArticle = {
+        title: "Test new article",
+        style: "Listicle",
+        content: "Test new article content..."
+      };
+
+      it(`responds with 400 and an error message when the '${field}' is missing`, () => {
+        delete newArticle[field];
+
+        return supertest(app)
+          .post("/articles")
+          .send(newArticle)
+          .expect(400, {
+            error: { message: `Missing '${field}' in request body` }
+          });
+      });
     });
   });
 });
